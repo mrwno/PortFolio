d2dc8c278a94441db324a96ffe5abaea6b12af9f
Marwane Ouaret <mar.ouaret@gmail.com>
2019-04-15 15:51:22 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ let nav = document.querySelector('nav');
 let button1 = document.querySelector('.contact__button');
 let presentation = document.querySelector('.intro__presentation');
 
-alert("Ce portfolio est en cours de developpement mais vous pouvez jeter un coup d'oeil si vous le souhaitez (Mettre la fenêtre en 1366 x 750 si votre viewport est plus grand que ce format).");
+alert("Ce portfolio est en cours de developpement mais vous pouvez jeter un coup d'oeil si vous le souhaitez.");
 
 button1.addEventListener('click', () =>{
   nav.classList.toggle('is-open');
